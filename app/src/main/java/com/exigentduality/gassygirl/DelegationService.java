094fa6da5cb365399c7f271232821364d34f2ed9
sniperpon <11429394+sniperpon@users.noreply.github.com>
2022-09-02 16:16:46 -0500

    Created version five, ready for production

---
+++ app/src/main/java/com/exigentduality/gassygirl/DelegationService.java
@@ -0,0 +1,14 @@
+package com.exigentduality.gassygirl;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
